b986ab3809485471d6788798605745ca359ba5cf
Useless Machine <kailajones518@gmail.com>
2018-10-08 09:36:27 -0700

    Initial commit

---
+++ app/src/main/java/com/example/truefalsequiz/TrueFalseQuiz.java
@@ -0,0 +1,13 @@
+package com.example.truefalsequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrueFalseQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_true_false_quiz);
+    }
+}
